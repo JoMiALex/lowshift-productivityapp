7257922ce2c17b75b73a5ef56bfc4abdf0ef8ce6
JoMiAlex <lottjohnmichael@yahoo.com>
2025-02-04 17:46:05 -0800

    last

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,9 @@ import { FormLogIn } from "@/app/login/page"
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <FormLogIn />
+
+      {/*<FormLogIn /> John Michael Test Page */ }
+      
     </main>
   )
 }
